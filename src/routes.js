985ba22d1765098b37e56dde79ef49a70ac1b1ef
GvictorFranca <57845536+GvictorFranca@users.noreply.github.com>
2020-10-21 15:46:14 -0300

    feat: login route

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 const Router = require('koa-router');
 const logger = require('./middlewares/logger');
 const Hello = require('./controllers/hello');
+const Users = require('./controllers/users');
 
 const router = new Router();
 
 router.get('/hello', Hello.hello);
-router.get('/hello2', Hello.hello);
+router.get('/user/:id', Users.getUser);
 
 router.get('/logger', logger, (ctx) => {
 	ctx.body = 'Eu pasei pelo logger';
